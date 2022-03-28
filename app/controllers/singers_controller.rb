84a438fded2813b79ce1e5c1adb23979697662ec
eric zarazir <eric.zarazir@gmail.com>
2022-03-28 15:58:09 +0200

    singers_controller

--- app/controllers/singers_controller.rb
+++ app/controllers/singers_controller.rb
@@ -1,22 +1,31 @@
 class SingersController < ApplicationController
-
   def index
-
+    @singers = Singer.all
   end
 
   def create
-
+    @singer = Singer.new(singer_params)
   end
 
   def show
-
+    @singer = Singer.find(params[:id])
   end
 
   def new
-
+    @singer = Singer.new
   end
 
   def edit
+    singer = Singer.find(params[:id])
+    if singer.destroy
+    else
+      error = { detail: "Artist with id #{params[:id]} cannot be deleted" }
+    end
   end
 
+  private
+
+  def singer_params
+    params.require(:singer).permit(:name, :image_url)
+  end
 end
