19281b10e1e33ed1ce3e0e1a5dec062a05bea825
call-042PE <clem13790@gmail.com>
2022-03-28 15:07:14 +0200

    User Model

---
+++ db/migrate/20220328124748_add_name_and_rating.rb
@@ -0,0 +1,7 @@
+class AddNameAndRating < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_reference :users, :rating, index: true
+  end
+end
