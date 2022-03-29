115f1dd840d917bb413f084c9496dee80b4a4fb1
thibaultbrianceau <tbrianceau@gmail.com>
2022-03-29 11:41:45 +0200

    seed et model a jours

--- db/migrate/20220329093932_create_singers.rb
+++ db/migrate/20220329093932_create_singers.rb
@@ -4,11 +4,10 @@ class CreateSingers < ActiveRecord::Migration[6.1]
       t.string :name
       t.string :category
       t.integer :price
-      t.text :bio
+      t.references :user, null: false, foreign_key: true
       t.integer :death_year
       t.integer :birth_year
-      t.references :user, null: false, foreign_key: true
-      t.references :booking, null: false, foreign_key: true
+      t.text :bio
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_28_125445) do
+ActiveRecord::Schema.define(version: 2022_03_29_093932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,14 +27,12 @@ ActiveRecord::Schema.define(version: 2022_03_28_125445) do
     t.string "name"
     t.string "category"
     t.integer "price"
-    t.text "bio"
+    t.bigint "user_id", null: false
     t.integer "death_year"
     t.integer "birth_year"
-    t.bigint "user_id", null: false
-    t.bigint "booking_id", null: false
+    t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["booking_id"], name: "index_singers_on_booking_id"
     t.index ["user_id"], name: "index_singers_on_user_id"
   end
 
@@ -54,6 +52,5 @@ ActiveRecord::Schema.define(version: 2022_03_28_125445) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "singers", "bookings"
   add_foreign_key "singers", "users"
 end
