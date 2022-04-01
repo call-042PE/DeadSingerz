5d6e1a08adadb07f21b873dab8dd95069d9e4a2b
Mr. Anderson <clem13790@gmail.com>
2022-04-01 15:23:01 +0200

    Update dashboard_controller.js

--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -92,7 +92,7 @@ export default class extends Controller {
               bErased = true;
             }
             this.userbookingTarget.insertAdjacentHTML("beforeend", `
-            <div class="card card-background text-center" style="width: 22rem; background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('http://res.cloudinary.com/ddkm6bg5l/image/upload/v1/development/${singer.key}')">
+            <div class="card card-background text-center" style="width: 22rem; background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('http://res.cloudinary.com/ddkm6bg5l/image/upload/v1/production/${singer.key}')">
               <div class="column">
                 <div class="card-body">
                   <h5 class="card-title">${singer.name}</h5>
